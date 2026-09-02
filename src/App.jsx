4f3f55db78d7909d6a17b967fc9a2c81fba96075
vanessa3rose <vanessa3rose@gmail.com>
2026-09-02 01:06:31 -0400

    pdf is page/download responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import './App.css'
 import { useState } from 'react'
 import { colors } from './assets/colors.js'
 
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/TextLayer.css';
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
+
 import Instagram from './assets/instagram-logo.png'
 import Facebook from './assets/facebook-logo.png'
 import Logo from './assets/roses-logo-rect.png'
@@ -9,6 +16,7 @@ import Menu from './assets/Menu_07-26.pdf'
 
 function App() {
 
+  // HOURS
   const [hoursExpanded, setHoursExpanded] = useState(false);
   const today = new Date();
   const days = ["SUN",  "MON", "TUE", "WED", "THU", "FRI", "SAT"];
@@ -21,6 +29,13 @@ function App() {
     "11:00 am — 3:00 pm",
     "Closed"
   ]
+
+  // PDF
+  const [currPage, setCurrPage] = useState(1);
+  const [numPages, setNumPages] = useState(null);
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
 
   return (
     <div className='flex flex-col'>
@@ -43,15 +58,59 @@ function App() {
           <div className='flex flex-1 bg-gray-400 h-px'/>
           <p className='flex px-4 text-sm font-medium text-gray-600'>Our Menu</p>
           <div className='flex flex-1 bg-gray-400 h-px'/>
-        </div>
+        </div>        
+        
+        {/* download */}
+        <a 
+          href={Menu} 
+          download="Menu_07-26.pdf" 
+          className='flex flex-row gap-x-1 justify-center items-center'
+        >
+          <p className='text-sm text-roses-red'>Download PDF</p>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill={colors.rosesRed} className="size-4 mt-0.5">
+            <path d="M8.75 2.75a.75.75 0 0 0-1.5 0v5.69L5.03 6.22a.75.75 0 0 0-1.06 1.06l3.5 3.5a.75.75 0 0 0 1.06 0l3.5-3.5a.75.75 0 0 0-1.06-1.06L8.75 8.44V2.75Z" />
+            <path d="M3.5 9.75a.75.75 0 0 0-1.5 0v1.5A2.75 2.75 0 0 0 4.75 14h6.5A2.75 2.75 0 0 0 14 11.25v-1.5a.75.75 0 0 0-1.5 0v1.5c0 .69-.56 1.25-1.25 1.25h-6.5c-.69 0-1.25-.56-1.25-1.25v-1.5Z" />
+          </svg>
+        </a>
 
         {/* view */}
-        <div className="w-full h-[65vh] sm:h-[85vh] min-h-100">
-          <iframe
-            src={`${Menu}#toolbar=1&navpanes=1&view=FitH`}
-            title="Menu PDF"
-            className="w-full h-full border-0"
-          />
+        <div className="flex h-full justify-center items-center overflow-hidden">
+          <Document file={Menu} onLoadSuccess={onDocumentLoadSuccess}>
+            <Page 
+              pageNumber={currPage} 
+              renderTextLayer={false} 
+              renderAnnotationLayer={false} 
+              className="border-3 border-taupe-200 [&>canvas]:max-w-full! [&>canvas]:h-auto!"
+            />
+          </Document>
+        </div>
+
+        {/* pages */}
+        <div className='flex flex-row gap-x-6 justify-center items-center'>
+          {/* < */}
+          {(currPage !== 1 && numPages !== 1) && (
+            <button className='flex flex-row gap-x-0.5 justify-center items-center' onClick={() => setCurrPage(currPage - 1)}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill={colors.rosesRed} className="size-4">
+                <path fillRule="evenodd" d="M9.78 4.22a.75.75 0 0 1 0 1.06L7.06 8l2.72 2.72a.75.75 0 1 1-1.06 1.06L5.47 8.53a.75.75 0 0 1 0-1.06l3.25-3.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
+              </svg>
+              <p className='text-xs text-roses-red font-semibold'>PREV</p>
+            </button>
+          )}
+
+          {/* page count */}
+          <div>
+            <p>{currPage} / {numPages} </p>
+          </div>
+
+          {/* > */}
+          {(currPage !== numPages) && (
+            <button className='flex flex-row gap-x-0.5 justify-center items-center' onClick={() => setCurrPage(currPage + 1)}>
+              <p className='text-xs text-roses-red font-semibold'>NEXT</p>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill={colors.rosesRed} className="size-4">
+                <path fillRule="evenodd" d="M6.22 4.22a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06l-3.25 3.25a.75.75 0 0 1-1.06-1.06L8.94 8 6.22 5.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+              </svg>
+            </button>
+          )}
         </div>
       </div>
 
@@ -99,7 +158,7 @@ function App() {
           {hoursExpanded ? (
             <div className='flex flex-col pb-5'>
               {hours.map((hrs, index) => (
-                <div className='flex flex-row gap-x-2'>
+                <div className='flex flex-row gap-x-2' key={index}>
                   <p className={`text-right text-roses-red ${index === today.getDay() ? 'w-[3.7ch] font-bold' : 'w-[4ch]'}`}>{days[index]}</p>
                   <p className={`text-left ${index === today.getDay() && "font-bold"}`}>{hrs}</p>
                 </div>
@@ -108,7 +167,7 @@ function App() {
           ) : (
             <div className='flex flex-col pb-5'>
               {hours.filter((_, index) => today.getDay() === index).map((hrs, index) => (
-                <div className='flex flex-row gap-x-2'>
+                <div className='flex flex-row gap-x-2' key={index}>
                   <p className='text-right text-roses-red'>Today</p>
                   <p className='text-left'>{hrs}</p>
                 </div>
